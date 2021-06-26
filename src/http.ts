505a9bf385274e94d5ac7112ae9a6bb6d3d6fc23
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-06-25 21:29:19 -0300

    feat: create and config database

--- src/http.ts
+++ src/http.ts
@@ -2,10 +2,16 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import path from "path";
+import mongoose from "mongoose";
 
 const app = express();
 
 const server = createServer(app);
+
+mongoose.connect("mongodb://localhost/igniteWeSocket", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const io = new Server(server);
 
